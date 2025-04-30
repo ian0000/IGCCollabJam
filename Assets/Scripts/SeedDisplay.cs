0b03a76fa66e5f5c5c5688fbcb88ab95f0bac088
Slurp <sherwoodwt@gmail.com>
2025-04-29 22:58:25 -0700

    Double digit seed display

--- Assets/Scripts/SeedDisplay.cs
+++ Assets/Scripts/SeedDisplay.cs
@@ -4,25 +4,33 @@ using UnityEngine.UI;
 public class SeedDisplay : MonoBehaviour {
     // These are in order
     [SerializeField] Sprite[] _digits;
-
-    // may have either one
-    SpriteRenderer _spriteRenderer;
-    Image _image;
+    [SerializeField] Image _singleDigit, _ones, _tens;
 
     void Start() {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _image = GetComponent<Image>();
         SeedManager.Instance.onSeedsChanged += UpdateDisplay;
         UpdateDisplay();
     }
 
     void UpdateDisplay() {
-        var digit = _digits[SeedManager.Instance.currentSeeds];
-        if (_spriteRenderer != null)
-            _spriteRenderer.sprite = digit;
-        if (_image != null)
-            _image.sprite = digit;
+        var digitCount = _digits.Length;
+        var seeds = SeedManager.Instance.currentSeeds;
+        if (seeds > digitCount)
+        {
+            _singleDigit.gameObject.SetActive(false);
+            _ones.gameObject.SetActive(true);
+            _tens.gameObject.SetActive(true);
 
+            _ones.sprite = _digits[seeds % digitCount];
+            _tens.sprite = _digits[seeds / digitCount];
+        }
+        else
+        {
+            _singleDigit.gameObject.SetActive(true);
+            _ones.gameObject.SetActive(false);
+            _tens.gameObject.SetActive(false);
+
+            _singleDigit.sprite = _digits[seeds];
+        }
     }
 
     void OnDestroy() {
